ad5f77f3a37beddefd84b7ea9cd4458756020141
Nuss93 <38286277+Nuss93@users.noreply.github.com>
2021-09-21 10:25:55 +0000

    ADDED ENV

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import discord
+import os
 
-print('well well look whos inside again')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+# my_secret = os.environ['TOKEN']
+client.run(os.getenv('TOKEN'))
